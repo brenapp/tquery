3ba48c83090177634c7cd3fc2932adad1ef184a0
Brendan McGuire <brendan@bren.app>
2024-12-26 17:40:10 -0600

    Add query and where statement

---
+++ src/parts/Query.ts
@@ -0,0 +1,9 @@
+import type { QueryKind } from "../utilities/QueryKind";
+import type { WhereStatement } from "./Where";
+
+export type Query<SelectStatement extends QueryKind<"SELECT">, Clauses extends QueryKind<"WHERE_CLAUSE">[] = []> = {
+    _brand: "QUERY",
+    SQL: `${SelectStatement["SQL"]}${WhereStatement<Clauses>["SQL"]}`
+    Result: SelectStatement["Result"],
+    BindParameters: WhereStatement<Clauses>["BindParameters"]
+};
